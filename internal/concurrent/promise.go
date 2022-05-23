90fa87e88decde0b6f491a106701d2d9cce09ba2
Camilo Roca <roc1fr@bosch.com>
2022-05-23 21:56:00 +0100

    feat: add a basic Promise type

---
+++ internal/concurrent/promise.go
@@ -0,0 +1,63 @@
+package concurrent
+
+import (
+	"sync"
+
+	"golang.org/x/sync/errgroup"
+)
+
+type Promise[T any] struct {
+	IsValid bool
+	Error   error
+	Value   T
+	wg      sync.WaitGroup
+}
+
+func NewPromise[T any](effect func() (T, error)) *Promise[T] {
+	return NewPromiseGroup(&errgroup.Group{}, effect)
+}
+
+func NewPromiseGroup[T any](group *errgroup.Group, effect func() (T, error)) *Promise[T] {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	promise := &Promise[T]{wg: wg}
+	group.Go(func() error {
+		defer promise.wg.Done()
+
+		value, err := effect()
+		promise.IsValid = true
+		if err != nil {
+			promise.Error = err
+			return err
+		}
+
+		promise.Value = value
+		return nil
+	})
+
+	return promise
+}
+
+func (promise *Promise[T]) Wait() (T, error) {
+	promise.wg.Wait()
+	return promise.Value, promise.Error
+}
+
+func PromiseAll[T any](promises ...*Promise[T]) *Promise[[]T] {
+	return PromiseAllGroup(&errgroup.Group{}, promises...)
+}
+
+func PromiseAllGroup[T any](group *errgroup.Group, promises ...*Promise[T]) *Promise[[]T] {
+	return NewPromiseGroup(group, func() ([]T, error) {
+		result := make([]T, len(promises))
+		for _, p := range promises {
+			v, err := p.Wait()
+			if err != nil {
+				return nil, err
+			}
+
+			result = append(result, v)
+		}
+		return result, nil
+	})
+}
