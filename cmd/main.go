eda2e8d1d9fa6ebd152168782d27b3a7acdb5a34
Camilo Roca <roc1fr@bosch.com>
2022-05-25 21:48:50 +0100

    first sketch of cli functionality

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bake/internal"
+	"bake/internal/lang"
 	"bake/internal/state"
+	"fmt"
 	"os"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclparse"
+	"github.com/urfave/cli"
 )
 
 func main() {
@@ -25,9 +28,13 @@ func main() {
 	}
 
 	addrs, diags := internal.ReadRecipes(cwd, parser)
-	if err != nil {
+	if diags.HasErrors() {
 		Fatal(log, diags)
 	}
+
+	app := App(addrs)
+	err = app.Run(os.Args)
+	os.Exit(2)
 
 	config := state.NewConfig(cwd)
 	config.Task = "main" // TODO
@@ -35,6 +42,50 @@ func main() {
 	if diags.HasErrors() {
 		Fatal(log, diags)
 	}
+}
+
+const (
+	DryRun = "dry-run"
+	List   = "list"
+	Prune  = "prune"
+	// Watch  = "watch" TODO
+)
+
+func App(addrs []lang.RawAddress) *cli.App {
+	app := &cli.App{
+		Name:  "bake",
+		Usage: "Build task orchestration",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:  List,
+				Usage: "List all public tasks",
+			},
+			&cli.BoolFlag{
+				Name:  Prune,
+				Usage: "Remove all files created by the current recipes",
+			},
+		},
+	}
+
+	commands := make(cli.Commands, 0)
+	for _, addr := range addrs {
+		if lang.IsKnownPrefix(addr.GetPath()) {
+			continue
+		}
+
+		cmd := cli.Command{
+			Name:  addr.GetName(),
+			Usage: addr.GetFilename(),
+			Action: func(c *cli.Context) error {
+				fmt.Println("added task: ", c.Args().First())
+				return nil
+			},
+		}
+		commands = append(commands, cmd)
+	}
+
+	app.Commands = commands
+	return app
 }
 
 func Fatal(log hcl.DiagnosticWriter, diagnostics hcl.Diagnostics) {
--- internal/lang/schema.go
+++ internal/lang/schema.go
@@ -32,10 +32,22 @@ var (
 	DataPrefix  = cty.GetAttrPath(DataLabel)
 	LocalPrefix = cty.GetAttrPath(LocalScope)
 	PathPrefix  = cty.GetAttrPath(PathScope)
+	// KnownPrefixes are the prefixes assigned to anything that is NOT a task
+	KnownPrefixes = cty.NewPathSet(DataPrefix, LocalPrefix, PathPrefix)
 	// GlobalPrefixes are those automatically injected by bake instead of defined by
 	// user input
 	GlobalPrefixes = cty.NewPathSet(PathPrefix)
 )
+
+func IsKnownPrefix(path cty.Path) bool {
+	for _, prefix := range KnownPrefixes.List() {
+		if path.HasPrefix(prefix) {
+			return true
+		}
+	}
+
+	return false
+}
 
 func RecipeSchema() *hcl.BodySchema {
 	return &hcl.BodySchema{
