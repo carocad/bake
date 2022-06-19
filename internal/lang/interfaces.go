5e501f43a42b3375a3b07944451bb540daa27723
carocad <carocad@unal.edu.co>
2022-06-19 20:32:05 +0200

    cosmetic changes

--- internal/lang/interfaces.go
+++ internal/lang/interfaces.go
@@ -1,6 +1,7 @@
 package lang
 
 import (
+	"bake/internal/concurrent"
 	"bake/internal/lang/config"
 	"bake/internal/lang/schema"
 	"bake/internal/lang/values"
@@ -13,6 +14,7 @@ import (
 	"github.com/hashicorp/hcl/v2/gohcl"
 	"github.com/mitchellh/colorstring"
 	"github.com/zclconf/go-cty/cty"
+	"github.com/zclconf/go-cty/cty/function/stdlib"
 )
 
 type Address interface {
@@ -79,4 +81,45 @@ func NewLogger(addr Address) *log.Logger {
 	prefix := colorstring.Color("[bold]" + AddressToString(addr))
 	// todo: change stdout according to state
 	return log.New(os.Stdout, prefix+": ", 0)
+}
+
+type Actions []Action
+
+func (actions Actions) Context() map[string]cty.Value {
+	variables := map[string]cty.Value{}
+	data := map[string]cty.Value{}
+	local := map[string]cty.Value{}
+	task := map[string]cty.Value{}
+	for _, act := range actions {
+		path := act.GetPath()
+		value := act.CTY()
+		var err error
+		switch {
+		case path.HasPrefix(schema.DataPrefix):
+			name := paths.StepString(path[1])
+			data[name] = value
+		case path.HasPrefix(schema.LocalPrefix):
+			name := paths.StepString(path[1])
+			local[name] = value
+		default:
+			name := paths.StepString(path[0])
+			val, ok := task[name] // if the key already exist it is a map
+			if ok {
+				value, err = stdlib.Merge(value, val)
+				if err != nil {
+					panic(err.Error())
+				}
+			}
+
+			task[name] = value
+		}
+	}
+
+	variables[schema.DataLabel] = cty.ObjectVal(data)
+	variables[schema.LocalScope] = cty.ObjectVal(local)
+	variables[schema.TaskLabel] = cty.ObjectVal(task)
+	// allow tasks to be referred without a prefix
+	concurrent.Merge(variables, task)
+
+	return variables
 }
--- internal/module/coordinator.go
+++ internal/module/coordinator.go
@@ -4,9 +4,7 @@ import (
 	"bake/internal/concurrent"
 	"bake/internal/lang"
 	"bake/internal/lang/config"
-	"bake/internal/lang/schema"
 	"bake/internal/module/topo"
-	"bake/internal/paths"
 	"context"
 	"fmt"
 	"path/filepath"
@@ -14,7 +12,6 @@ import (
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/zclconf/go-cty/cty"
-	"github.com/zclconf/go-cty/cty/function/stdlib"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -117,7 +114,7 @@ func (coordinator *Coordinator) Do(state *config.State, task lang.RawAddress, ad
 		evalContext := state.EvalContext()
 		evalContext.Variables = concurrent.Merge(
 			pathContext(state, address),
-			actionsContext(coordinator.actions.Items()),
+			lang.Actions(coordinator.actions.Items()).Context(),
 		)
 		actions, diags := address.Decode(evalContext)
 		if diags.HasErrors() {
@@ -180,43 +177,4 @@ func pathContext(state *config.State, addr lang.Address) map[string]cty.Value {
 			"current": cty.StringVal(filepath.Join(cwd, addr.GetFilename())),
 		}),
 	}
-}
-
-func actionsContext(actions []lang.Action) map[string]cty.Value {
-	variables := map[string]cty.Value{}
-	data := map[string]cty.Value{}
-	local := map[string]cty.Value{}
-	task := map[string]cty.Value{}
-	for _, act := range actions {
-		path := act.GetPath()
-		value := act.CTY()
-		var err error
-		switch {
-		case path.HasPrefix(schema.DataPrefix):
-			name := paths.StepString(path[1])
-			data[name] = value
-		case path.HasPrefix(schema.LocalPrefix):
-			name := paths.StepString(path[1])
-			local[name] = value
-		default:
-			name := paths.StepString(path[0])
-			val, ok := task[name] // if the key already exist it is a map
-			if ok {
-				value, err = stdlib.Merge(value, val)
-				if err != nil {
-					panic(err.Error())
-				}
-			}
-
-			task[name] = value
-		}
-	}
-
-	variables[schema.DataLabel] = cty.ObjectVal(data)
-	variables[schema.LocalScope] = cty.ObjectVal(local)
-	variables[schema.TaskLabel] = cty.ObjectVal(task)
-	// allow tasks to be referred without a prefix
-	concurrent.Merge(variables, task)
-
-	return variables
 }
